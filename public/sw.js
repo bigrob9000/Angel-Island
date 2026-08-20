5b1403921a2e0e94e2b8f89817708ff2ec96e98d
Rob <rob@angelislandconnect.com>
2026-08-19 23:18:47 -0500

    Update site logo to navy and gold AI mark.
    Replace logo assets with new artwork blended to the site background; bump PWA cache for icon refresh.

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "angel-island-shell-v4";
+const CACHE = "angel-island-shell-v5";
 const PRECACHE = ["/", "/manifest.webmanifest", "/angel-island-mark-light.png"];
 
 self.addEventListener("install", (event) => {
